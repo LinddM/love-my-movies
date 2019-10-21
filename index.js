23a0352e999bdf7761aa3dfbf3531a0dde78a9b8
Maite de la Roca <delaroca.estrada21@gmail.com>
2019-10-21 18:18:50 -0500

    manage votes

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const Mustache = require('mustache')
 const fs = require('fs')
 const axios = require('axios');
 
+var redis = require('redis');
+var client = redis.createClient(6379, '192.168.99.100');
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+//  create key and set value
+client.set("string key", "xxx", redis.print);
+
 
 app.get('/', function (req, res) {
     const template = fs.readFileSync('templates/card.html', 'utf8');
@@ -14,14 +24,17 @@ app.get('/', function (req, res) {
     // Make a request for a user with a given ID
     axios.get('https://api.themoviedb.org/3/discover/movie?api_key=4fc52af49a78473fd64a96b128b0f0cc&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
         .then(function (response) {
+            let movies = response.data.results
+            for (let i = 0; i< response.data.results.length; i++) {
+                client.setnx(response.data.results[i].id, 0, redis.print);
+            }
+            
             // handle success
-            console.log(response.data.results);
-            const movies = response.data.results
+            console.log(movies);            
             const renderedCards = Mustache.render(template, {movies: movies})
             const renderedIndex = Mustache.render(index, {'cards': renderedCards})
 
-            res.status(200).send(renderedIndex)
-            
+            res.status(200).send(renderedIndex)            
         })
         .catch(function (error) {
             // handle error
@@ -32,7 +45,24 @@ app.get('/', function (req, res) {
         });
 
     
-  })
+  }
+)
+
+app.patch('/movie/:movie/:vote', function (req, res) {
+    const movie = req.params.movie
+    const vote = req.params.vote
+    
+    client.get(movie, (err, reply) => {
+        if (err) {
+            console.error(err)
+            res.status(500).send(false)
+        }
+        console.log(reply)
+        client.set(movie, parseInt(reply) + parseInt(vote), redis.print);
+        
+        res.status(200).send(true)
+    })
+})
 
 app.use(express.static(path.join(__dirname, 'templates')))
  
