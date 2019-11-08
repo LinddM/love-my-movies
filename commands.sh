4c1df57d296ccf1c980acbb6dfeabe22a7d8d9b5
Maite de la Roca <delaroca.estrada21@gmail.com>
2019-11-07 20:14:59 -0600

    commands.sh updated

--- commands.sh
+++ commands.sh
@@ -1,2 +1,4 @@
 docker pull redis
-docker run -d --name redisDev -p 6379:6379 redis
+docker run -d --name mov -p 6379:6379 redis
+docker pull lindseymaite/love-my-movies:3.1
+docker run -it -p 3000:3000 lindseymaite/love-my-movies:3.1
